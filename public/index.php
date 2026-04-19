540329f3ff29e01b25dd40d06f1c767dab2feeda
Jose <jriverar1@unicartagena.edu.co>
2026-04-19 12:09:13 -0500

    feat: cambio para solucionar errores en vercel

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 require_once __DIR__ . '/../config/database.php';
 
